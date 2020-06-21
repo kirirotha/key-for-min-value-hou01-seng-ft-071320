edc280faf0ae878218d2b2ba0526c01def907b54
Kiri Um <kirirotha@gmail.com>
2020-06-21 13:32:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_val_key = 0
+  min_val_key = ""
   if name_hash == nil
     return nil
   end
