e3a3cd1db460c189a108f7a7c5e047827b134ee6
Kiri Um <kirirotha@gmail.com>
2020-06-21 13:25:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   min_val = 999999999
   name_hash.each do |key, element|
     if element < min_val
-      min_val = element
+      min_val_key = key
     end
   end
   puts min_val
